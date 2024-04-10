1b6915592f8e02598a28714f24cbb70224f48f44
kmargg <157771266+kmargg@users.noreply.github.com>
2024-04-09 20:55:20 -0700

    Refactor to newer react syntax

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,62 +1,62 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Formik } from 'formik';
 import { Row, Col } from 'reactstrap';
-
-
 import HomeHeader from '../home/Header';
-import Footer from '../home/Footer'
+import Footer from '../home/Footer';
 
-class Login extends Component {
-  render() {
-    return (
-      <div>
-        <section className="container-fluid template-main pad-none responsive-screen">
+function Login() {
+  // Normally we would use useState here if we need to manage state outside of Formik
+
+  const validate = values => {
+    const errors = {};
+    if (!values.email) {
+      errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+      errors.email = 'Invalid email address';
+    }
+    return errors;
+  };
+
+  const handleSubmit = (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 400);
+  };
+
+  return (
+    <div>
+      <section className="container-fluid template-main pad-none responsive-screen">
         <HomeHeader />
         <div id="content">
           <div className="container">
-           <div className="row margin-none">
-             <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center responsive-screen-block">
-               <div className="container">
-                 <div className="row faq-banner">
-                   <div>
-                     <h1>LOGIN</h1>
-                   </div>
-                 </div>
-               </div>
-             </div>
+            <div className="row margin-none">
+              <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center responsive-screen-block">
+                <div className="container">
+                  <div className="row faq-banner">
+                    <div>
+                      <h1>LOGIN</h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 dashboard-inner pad-none">
-                <div className="template-detail-box" style={{ color: '#fff' }}>
-                  <div style={{ margin: '20'}}>
-                    <Formik
-                      initialValues={{ email: '', password: '' }}
-                      validate={values => {
-                        let errors = {};
-                        if (!values.email) {
-                          errors.email = 'Required';
-                        } else if (
-                          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                        ) {
-                          errors.email = 'Invalid email address';
-                        }
-                        return errors;
-                      }}
-                      onSubmit={(values, { setSubmitting }) => {
-                        setTimeout(() => {
-                          alert(JSON.stringify(values, null, 2));
-                          setSubmitting(false);
-                        }, 400);
-                      }}
-                    >
-                      {({
-                        values,
-                        errors,
-                        touched,
-                        handleChange,
-                        handleBlur,
-                        handleSubmit,
-                        isSubmitting,
-                      }) => (
-                        <form onSubmit={handleSubmit}>
+                <div className="template-detail-box" style={{ margin: '20px' }}>
+                  <Formik
+                    initialValues={{ email: '', password: '' }}
+                    validate={validate}
+                    onSubmit={handleSubmit}
+                  >
+                    {({
+                      values,
+                      errors,
+                      touched,
+                      handleChange,
+                      handleBlur,
+                      handleSubmit,
+                      isSubmitting,
+                    }) => (
+                      <form onSubmit={handleSubmit}>
                         <Row>
                           <Col xs={9} sm={4} md={3} style={{ marginTop: 15 }}>
                             <label>Email</label><br/>
@@ -90,20 +90,18 @@ class Login extends Component {
                             </button>
                           </Col>
                         </Row>
-                        </form>
-                      )}
-                    </Formik>
-                  </div>
+                      </form>
+                    )}
+                  </Formik>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        </section>
-        <Footer />
-      </div>
-    );
-  }
+      </section>
+      <Footer />
+    </div>
+  );
 }
 
 export default Login;
